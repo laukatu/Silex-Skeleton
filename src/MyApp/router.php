2186baebe948b394c5b28a6864308eae512da4a1
Luis Lorenzo <info@laukatu.com>
2015-08-19 11:46:06 +0200

    Fixed routing error pages bug

--- src/MyApp/router.php
+++ src/MyApp/router.php
@@ -2,15 +2,13 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception;
 
 use MyApp\Controller;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
-$app->error(function (\Exception $e, $code) use ($app) {
+$app->error(function (\Exception $e, Request $request, $code) use ($app) {
     if ($app['debug']) {
         return;
     }
